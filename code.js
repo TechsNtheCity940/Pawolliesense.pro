113c0931c594ddfcd073bebbe8c5395212920910
TechsNtheCity940 <182389709+TechsNtheCity940@users.noreply.github.com>
2026-01-30 17:05:24 -0600

    Remove code for generating code verifier and challenge

--- code.js
+++ code.js
@@ -1,10 +1,1 @@
-import crypto from "crypto";
 
-const codeVerifier = crypto.randomBytes(96).toString("base64url");
-const codeChallenge = crypto
-  .createHash("sha256")
-  .update(codeVerifier)
-  .digest("base64url");
-
-console.log("code_verifier:", codeVerifier);
-console.log("code_challenge:", codeChallenge);
